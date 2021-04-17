e8100f791e062b98fee94d422ae048ddd0b2a940
Vinicius Mazon <vnpmazon@gmail.com>
2021-04-17 10:08:08 -0300

    add skeleton effect on filter card

--- src/app/components/admin/card-stats/card-stats.component.ts
+++ src/app/components/admin/card-stats/card-stats.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardStatsComponent implements OnInit {
 
-  public isLoading = true;
+  public isLoading = false;
 
   @Input()
   label: string;
